53ce20366e94279e0bf6f7f98ea66e7183366a11
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-04-28 16:47:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+song_1 = "";
+song_2 = "";
+
+function preload(){
+  song_1 = loadSound("music.mp3")
+  song_2 = loadSound("music2.mp3")
+}
+
 function setup(){
-    canvas = createCanvas(600,530);
+    canvas = createCanvas(600,500);
     canvas.center();
+
+    video = createCapturee(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
 }
