0af7edf7ab4667d1bc09be2c23212d109b9a007a
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-05-12 14:54:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 
+ScoreLeftWrist = 0;
+
 function preload(){
   song_1 = loadSound("music.mp3")
   song_2 = loadSound("music2.mp3")
@@ -16,7 +18,7 @@ function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
 
-    video = createCapturee(VIDEO);
+    video = createCapture(VIDEO);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -25,6 +27,18 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("red");
+    stroke("red");
+    
+    if(ScoreLeftWrist > 0.2){
+      circle(leftWristX,leftWristY,20);
+      song_2.stop();
+      if(song_1.isPlaying()==false){
+      song_1.play();
+      document.getElementById("song").innerHTML = " Song Name : Harry Potter"
+      }
+    }
 }
 
 function modelLoaded(){
@@ -34,6 +48,7 @@ function modelLoaded(){
 function gotPoses(results){
   if(results.length > 0){
     console.log(results);
+    ScoreLeftWrist = results[0].pose.keypoints[9].score;
 
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
@@ -42,5 +57,7 @@ function gotPoses(results){
     rightWristX = results[0].pose.rightWrist.x;
     rightWristY = results[0].pose.rightWrist.y;
     console.log("rightWristX =" + rightWristX +" rightWristY = "+ rightWristY);
+
+
   }
 }
